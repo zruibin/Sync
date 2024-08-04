e1dc83d9cea041efe3aad2cdb677c9f06b49babc
ruibin.chow <ruibin.chow@qq.com>
2024-08-04 19:51:00 +0800

    Fix ignore error.

--- src/sync/ignore/ignore.cc
+++ src/sync/ignore/ignore.cc
@@ -158,7 +158,7 @@ bool GitIgnore::Accepts(const std::string_view input) const {
     }
     if (match) return false;
     
-    return false;
+    return true;
 }
     
 bool GitIgnore::Denies(const std::string_view input) const {
